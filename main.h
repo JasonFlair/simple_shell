b71fa1741f075ed09d1c2a7b060cd060682fc926
JasonFlair <emelieobumse100@gmail.com>
2022-08-05 20:15:00 +0100

    man

--- main.h
+++ main.h
@@ -26,7 +26,11 @@ int shell_exit();
 int shell_launch(char **args);
 int shell_execute(char **args);
 int shell_num_builtins();
-int (*builtin_func[]) (char **);
-extern char *builtin_str[256];
+int (*builtin_func[]) (char **) = {
+  &shell_cd,
+  &shell_help,
+  &shell_exit
+};
+char *builtin_str[256];
 
 #endif 
