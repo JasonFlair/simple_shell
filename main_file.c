90134b45b8362019534a4d6766d9c91589b9f4b8
Jason <emelieobumse100@gmail.com>
2022-08-11 14:06:38 -0700

    betty

--- main_file.c
+++ main_file.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ * main - runs the shell loop
  * shell_loop- a function causes a continous loop
  * until shell is exited.
  * @argc:argument count.
@@ -10,29 +11,28 @@
 
 int main(__attribute__ ((unused))int argc, __attribute__ ((unused))char **argv)
 {
-    shell_loop(); /* function to run in a loop */
+	shell_loop(); /* function to run in a loop */
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 void shell_loop() /* loop function */
 {
-    char *gotten_line;
-    char **args;
-    int status;
-    int isPipe = 0;
-    
-    do
-    {
-        if (isatty(fileno(stdin)))
-        {
-            printf("> ");
-            isPipe = 1;
-        }
-        gotten_line = read_line();
-        args = split_line(gotten_line);
-        status = shell_execute(args);
-        
-        free(gotten_line);
-        free(args);
-    } while (status && isPipe);
+	char *gotten_line;
+	char **args;
+	int status;
+	int isPipe = 0;
+
+	do
+	{
+		if (isatty(fileno(stdin)))
+		{
+			printf("> ");
+			isPipe = 1;
+		}
+		gotten_line = read_line();
+		args = split_line(gotten_line);
+		status = shell_execute(args);
+		free(gotten_line);
+		free(args);
+	} while (status && isPipe);
 }
