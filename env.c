b933ff837e5cb0ef51fdd8f9a9261ca1ab23e202
Jason <emelieobumse100@gmail.com>
2022-08-11 16:05:03 -0700

    b

--- env.c
+++ env.c
@@ -4,10 +4,10 @@ extern char **environ;
 
 void check_env(char **envp)
 {
-    int i;
-
-    for (i = 0; envp[i] != NULL; i++)
-    {
-        printf("%s\n", envp[i]);
-    }
+	int i;
+	
+	for (i = 0; envp[i] != NULL; i++)
+	{
+		printf("%s\n", envp[i]);
+	}
 } 
