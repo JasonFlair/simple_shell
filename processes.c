9f105473762e037404d57096c777070aa5eb0816
Jason <emelieobumse100@gmail.com>
2022-08-11 16:33:02 -0700

    b

--- processes.c
+++ processes.c
@@ -2,23 +2,31 @@
 
 int shell_launch(char **args)
 {
-    pid_t child_pid;
-    int status;
-
-    child_pid = fork();
-
-    if (child_pid == 0) {
-    if (execvp(args[0], args) == -1) /*if executuion fails*/ {
-      perror("shell error"); /* print error, else execute */
-      exit(EXIT_FAILURE);
-    }
-  } else if (child_pid < 0) { /* if no child process */
-    perror("nothing forked");
-  } else {
-    do {
-      waitpid(child_pid, &status, WUNTRACED);
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-  }
-
-  return 1;
+	pid_t child_pid;
+	int status;
+	
+	child_pid = fork();
+	
+	if (child_pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+			/*if executuion fails*/
+		{
+			perror("shell error");
+			/* print error, else execute */
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (child_pid < 0) 
+	{ /* if no child process */
+		perror("nothing forked");
+	}
+	else
+	{
+		do {
+			waitpid(child_pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	
+	return (1);
 }
