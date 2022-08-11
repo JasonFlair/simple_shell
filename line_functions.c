7cfffb87dbf786294bb564e1a453cfb7fd276658
Jason <emelieobumse100@gmail.com>
2022-08-11 14:47:24 -0700

    hm

--- line_functions.c
+++ line_functions.c
@@ -8,25 +8,25 @@
 
 char *read_line(void)
 {
-    char *line = NULL;
-    size_t bufsize = 0;
-
-    ssize_t getlinefunc = getline(&line, &bufsize, stdin); /* getline allocates memory */
-
-    if (getlinefunc == -1)
-    {
-        if (feof(stdin))
-        {
-            exit(EXIT_SUCCESS); /* we got an eof */
-        }
-        else
-        {
-            perror("readline?");
-            exit(EXIT_FAILURE);
-        }
-        
-    }
-    return line;
+	char *line = NULL;
+	size_t bufsize = 0;
+	
+	ssize_t getlinefunc = getline(&line, &bufsize, stdin); /* getline allocates memory */
+	
+	if (getlinefunc == -1)
+	{
+		if (feof(stdin))
+		{
+			exit(EXIT_SUCCESS); /* we got an eof */
+		}
+		else
+		{
+			perror("readline?");
+			exit(EXIT_FAILURE);
+		}
+	
+	}
+	return line;
 }
 
 /**
@@ -37,33 +37,32 @@ char *read_line(void)
 
 char **split_line(char *line)
 {
-    int bufsize = TOK_BUFSIZE, i = 0;
-    char **tokens = malloc(bufsize * sizeof(char*));
-    char *token;
-
-    if (!tokens)
-    {
-        fprintf(stderr, "shell: error in allocation\n");
-        exit(EXIT_FAILURE);
-    }
-
-    token = strtok(line, TOK_DELIM);
-    while (token != NULL)
-    {
-        tokens[i] = token;
-        i++;
-
-        if (i >= bufsize)
-        {
-            bufsize += TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens)
-            {
-             fprintf(stderr, "shell: error in allocation\n");
-            }
-        }
-        token = strtok(NULL, TOK_DELIM);
-    }
-    tokens[i] = NULL;
-    return tokens;
+	int bufsize = TOK_BUFSIZE, i = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token;
+	
+	if (!tokens)
+	{
+		fprintf(stderr, "shell: error in allocation\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	token = strtok(line, TOK_DELIM);
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		i++;
+		if (i >= bufsize)
+		{
+			bufsize += TOK_BUFSIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "shell: error in allocation\n");
+			}
+		}
+		token = strtok(NULL, TOK_DELIM);
+	}
+	tokens[i] = NULL;
+	return tokens;
 }
