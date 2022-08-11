2f4a9d0439154f862500a779c6412d3b0a4ec5ef
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-08-11 21:35:03 +0100

    Update main_file.c

--- main_file.c
+++ main_file.c
@@ -1,19 +1,26 @@
 #include "main.h"
 
+/**
+ * shell_loop- a function causes a continous loop
+ * until shell is exited.
+ * @arcs:argument count.
+ * @argv: array of pointers.
+ * Return: void.
+*/
+
 int main(__attribute__ ((unused))int argc, __attribute__ ((unused))char **argv)
 {
     shell_loop(); /* function to run in a loop */
 
     return EXIT_SUCCESS;
 }
-
 void shell_loop() /* loop function */
 {
     char *gotten_line;
     char **args;
     int status;
     int isPipe = 0;
-
+    
     do
     {
         if (isatty(fileno(stdin)))
@@ -24,7 +31,7 @@ void shell_loop() /* loop function */
         gotten_line = read_line();
         args = split_line(gotten_line);
         status = shell_execute(args);
-
+        
         free(gotten_line);
         free(args);
     } while (status && isPipe);
