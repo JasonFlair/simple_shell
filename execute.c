c0bd83bae29613c55d510e532fc81a550e6138ce
Jason <emelieobumse100@gmail.com>
2022-08-11 16:17:35 -0700

    h

--- execute.c
+++ execute.c
@@ -1,32 +1,34 @@
 #include "main.h"
 
 /**
- * shell_execute - a function to compare 
+ * shell_execute - a function to compare
  * and execute commands
  * @args: env pointer
  * Return: none.
-*/
+ */
 
 int shell_execute(char **args)
 {
-  int i;
-  builtins func[] = {
-  {"cd", shell_cd},
-  {"help", shell_help},
-  {"exit", shell_exit},
-  {NULL, NULL}
-};
-
-  if (args[0] == NULL) {
-    /* if an empty command was entered. */
-    return 1;
-  }
-
-  for (i = 0; func[i].str != NULL; i++) {
-    if (strcmp(args[0], func[i].str) == 0) {
-      return func[i].f(args);
-    }
-  }
-
-  return shell_launch(args);
+	int i;
+	
+	builtins func[] = {
+		{"cd", shell_cd},
+		{"help", shell_help},
+		{"exit", shell_exit},
+		{NULL, NULL}
+	};
+	
+	if (args[0] == NULL)
+	{
+		/* if an empty command was entered. */
+		return (1);
+	}
+	for (i = 0; func[i].str != NULL; i++)
+	{
+		if (strcmp(args[0], func[i].str) == 0)
+		{
+			return func[i].f(args);
+		}
+	}
+	return shell_launch(args);
 }
