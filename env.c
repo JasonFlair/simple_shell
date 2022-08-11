139b579544457e07ed924a6de6df83c71e767ac7
Jason <emelieobumse100@gmail.com>
2022-08-11 16:11:34 -0700

    k

--- env.c
+++ env.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
-extern char **environ;
-
 /**
  * check_env - a function to print 
  * the env
  * @envp: env pointer
  * Return: none.
-*/
+ */
 
 void check_env(char **envp)
 {
 	int i;
-	
+
 	for (i = 0; envp[i] != NULL; i++)
 	{
 		printf("%s\n", envp[i]);
