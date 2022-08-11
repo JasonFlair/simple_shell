fe2b6ca7fc42505c1c2ea2b95263b09f5d38ac69
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-08-11 23:48:33 +0100

    Update main.h

--- main.h
+++ main.h
@@ -11,21 +11,17 @@
 #define TOK_BUFSIZE 64
 #define TOK_DELIM " \t\r\n\a"
 
-char *gotten_line;
-char **args;
-int status;
-
 void shell_loop(void);
 char *read_line(void);
 char **split_line(char *line);
-void check_env(char ** envp);
+void check_env(char **envp);
 ssize_t getline();
 int shell_cd(char **args);
-int shell_help();
-int shell_exit();
+int shell_help(void);
+int shell_exit(void);
 int shell_launch(char **args);
 int shell_execute(char **args);
-int shell_num_builtins();
+int shell_num_builtins(void);
 char *builtin_str[256];
 
 typedef struct builtins
